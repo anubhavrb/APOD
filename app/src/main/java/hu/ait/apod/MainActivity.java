b2eced844e6af516af09f7bd2fa1be97a0155817
anubhavrb <anubhavrb@gmail.com>
2017-12-01 02:37:45 -0500

    Add date picker dialog

--- app/src/main/java/hu/ait/apod/MainActivity.java
+++ app/src/main/java/hu/ait/apod/MainActivity.java
@@ -1,7 +1,10 @@
 package hu.ait.apod;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,6 +18,7 @@ import java.util.Date;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import hu.ait.apod.data.APODResult;
 import hu.ait.apod.network.ApodAPI;
 import retrofit2.Call;
@@ -27,10 +31,24 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String API_KEY = "DoQByy5daS0Cf2hkVVWb2LFaYe7mmgrZyMak8CHT";
 
+    @BindView(R.id.btnDate)
+    Button btnDate;
     @BindView(R.id.tvData)
     TextView tvData;
     @BindView(R.id.imgAPOD)
     ImageView imgAPOD;
+
+    final Calendar myCalendar = Calendar.getInstance();
+
+    DatePickerDialog.OnDateSetListener datePicker = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, month);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            getAPODInfo(myCalendar.getTime());
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,14 +57,22 @@ public class MainActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
-        getAPODInfo(Calendar.getInstance().getTime());
-
+        getAPODInfo(myCalendar.getTime());
     }
 
-    private void getAPODInfo(Date today) {
+    @OnClick(R.id.btnDate)
+    public void btnClicked() {
+        new DatePickerDialog(MainActivity.this, datePicker,
+                myCalendar.get(Calendar.YEAR),
+                myCalendar.get(Calendar.MONTH),
+                myCalendar.get(Calendar.DAY_OF_MONTH))
+                .show();
+    }
+
+    private void getAPODInfo(Date date) {
         DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-        String date = formatter.format(today);
-        tvData.setText(date);
+        String date_s = formatter.format(date);
+        tvData.setText(date_s);
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://api.nasa.gov/planetary/")
@@ -55,18 +81,19 @@ public class MainActivity extends AppCompatActivity {
 
         ApodAPI apodAPI = retrofit.create(ApodAPI.class);
 
-        Call<APODResult> callAPOD = apodAPI.getAPOD(date, "true", API_KEY);
+        Call<APODResult> callAPOD = apodAPI.getAPOD(date_s, "true", API_KEY);
         callAPOD.enqueue(new Callback<APODResult>() {
             @Override
             public void onResponse(Call<APODResult> call, Response<APODResult> response) {
                 APODResult result = response.body();
                 if (result == null)
                     return;
-                tvData.setText(result.getExplanation());
 
                 Glide.with(MainActivity.this)
                         .load(result.getHdurl())
                         .into(imgAPOD);
+
+                tvData.setText(result.getExplanation());
             }
 
             @Override
